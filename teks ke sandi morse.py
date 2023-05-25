83043a81a5d0e6713be6756919b65859abd80553
Saka <gatotsasangkataka@gmail.com>
2023-05-25 21:58:21 +0700

    Ok

---
+++ teks ke sandi morse.py
@@ -0,0 +1,22 @@
+# Membuat kamus sandi Morse
+morse_code = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', ' ': '/'
+}
+
+# Fungsi untuk mengenkripsi teks menjadi sandi Morse
+def encrypt_morse(text):
+    encrypted_text = ''
+    for char in text:
+        char = char.upper()
+        if char in morse_code:
+            encrypted_text += morse_code[char] + ' '
+    return encrypted_text
+
+# Contoh penggunaan
+plaintext = "HELLO WORLD"
+encrypted_text = encrypt_morse(plaintext)
+print("Plaintext: ", plaintext)
+print("Encrypted Text (Morse Code): ", encrypted_text)
