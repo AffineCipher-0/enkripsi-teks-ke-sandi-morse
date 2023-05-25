e1bff12fd7a9cdb8007752db790afd757be6f840
Saka <gatotsasangkataka@gmail.com>
2023-05-25 22:19:38 +0700

    percobaan berhasil

--- ok.py
+++ ok.py
@@ -7,19 +7,22 @@ morse_code = {
     '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0', '/': ' '
 }
 
-# Fungsi untuk menerjemahkan sandi Morse ke teks 
+# Fungsi untuk menerjemahkan sandi Morse ke teks
+def morse_to_text(morse):
     morse_words = morse.split(' / ')
     translated_text = ''
     for word in morse_words:
         letters = word.split(' ')
         translated_word = ''
         for letter in letters:
-            if letter in morse_code:
-                translated_word += morse_code[letter]
+            for key, value in morse_code.items():
+                if letter == key:
+                    translated_word += value
+                    break
         translated_text += translated_word + ' '
     return translated_text.strip()
 
-# Fungsi untuk menerjemahkan teks ke sandi Morse
+# Fungsi untuk menerjemahkan teks ke sandi Morse 
 def text_to_morse(text):
     translated_morse = ''
     for char in text:
@@ -36,7 +39,7 @@ translated_text = morse_to_text(morse)
 print("Morse Code: ", morse)
 print("Translated Text: ", translated_text)
 
-plaintext = "Belajar"  # Teks biasa
+plaintext = "Percobaan berhasil"  # Teks biasa
 translated_morse = text_to_morse(plaintext)
 print("Plaintext: ", plaintext)
 print("Translated Morse Code: ", translated_morse)
